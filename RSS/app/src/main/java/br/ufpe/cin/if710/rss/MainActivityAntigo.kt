e4a52c23a4db39ad26e0d13489e6cd4097c7f9f7
Igor Matos <igor.matos@liferay.com>
2018-09-17 21:27:12 -0300

    Entrega

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivityAntigo.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivityAntigo.kt
@@ -2,11 +2,8 @@ package br.ufpe.cin.if710.rss
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.widget.TextView
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
@@ -14,10 +11,10 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.nio.charset.Charset
 
-class MainActivity : Activity() {
+class MainActivityAntigo : Activity() {
 
     //ao fazer envio da resolucao, use este link no seu codigo!
-    private lateinit var RSS_FEED: String
+    private val RSS_FEED = "http://leopoldomt.com/if1001/g1brasil.xml"
 
     //OUTROS LINKS PARA TESTAR...
     //http://rss.cnn.com/rss/edition.rss
@@ -26,44 +23,23 @@ class MainActivity : Activity() {
     //http://pox.globo.com/rss/g1/tecnologia/
 
     //use ListView ao invés de TextView - deixe o atributo com o mesmo nome
-    private lateinit var conteudoRSS: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-
+    private var conteudoRSS: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         conteudoRSS = findViewById(R.id.conteudoRSS)
-
-        viewManager = LinearLayoutManager(this)
-        RSS_FEED = getString(R.string.rssfeed)
     }
 
     override fun onStart() {
         super.onStart()
-
-        doAsync {
-            try {
-                val feedXML = getRssFeed(RSS_FEED)
-
-                val list = ParserRSS.parse(feedXML)
-
-                viewAdapter = ItemAdapter(list)
-
-                uiThread {
-                    conteudoRSS.apply {
-                        setHasFixedSize(true)
-                        layoutManager = viewManager
-                        adapter = viewAdapter
-                    }
-                }
-
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
+        try {
+            //Esse código dá pau, por fazer operação de rede na thread principal...
+            val feedXML = getRssFeed(RSS_FEED)
+            conteudoRSS!!.text = feedXML
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
-
 
     }
 
